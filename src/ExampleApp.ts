347d286083a41d9e7d68f94064d8f74a56e2de71
Daniel Keefe <dfk@umn.edu>
2023-10-11 14:32:32 -0500

    in class updates

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -22,7 +22,7 @@ export class ExampleApp extends gfx.GfxApp
         // initialize the base class gfx.GfxApp
         super();
 
-        this.wireframe = false;
+        this.wireframe = true;
         this.gui = new GUI({ width: 300, closed: false });
         this.gui.add(this, 'wireframe')
             .onChange((value) => {
@@ -60,7 +60,7 @@ export class ExampleApp extends gfx.GfxApp
         const axes = gfx.Geometry3Factory.createAxes(4);
         this.scene.add(axes);
 
-        this.cylinder = this.createCylinderMesh(20, 3);
+        this.cylinder = this.createCylinderMesh();
         this.scene.add(this.cylinder);
     }
 
@@ -70,16 +70,79 @@ export class ExampleApp extends gfx.GfxApp
         this.cameraControls.update(deltaTime);
     }
 
-    private createCylinderMesh(numSegments: number, height: number): gfx.Mesh3
+    private createCylinderMesh(): gfx.Mesh3
     {
         const mesh: gfx.Mesh3 = new gfx.Mesh3();
         const vertices: gfx.Vector3[] = [];
+        const normals: gfx.Vector3[] = [];
         const indices: number[] = [];
 
         // TODO: Fill in the vertices and indices arrays
 
+        /*
+        // example 1: two triangles
+        vertices.push(new gfx.Vector3(-1, -1, 0));
+        vertices.push(new gfx.Vector3(-1, 1, 0));
+        vertices.push(new gfx.Vector3(1, -1, 0));
+        vertices.push(new gfx.Vector3(1, 1, 0));
+
+        // triangle 1
+        //indices.push(0);
+        //indices.push(3);
+        //indices.push(1);
+
+        // triangle 1
+        indices.push(0, 3, 1);
+        indices.push(0, 2, 3);
+        */
+
+        /*
+        // example 2: bars
+        const nBars = 10;
+        for (let i=0; i<=nBars; i++) {
+            // a goes 0..1 across the width
+            const a = i / nBars;
+            const xStart = -1;
+            const xEnd = 1;
+            const xRange = xEnd - xStart;
+            const x = xStart + a * xRange;
+            vertices.push(new gfx.Vector3(x, -1, 0));
+            vertices.push(new gfx.Vector3(x, 1, 0));
+        }
+
+        for (let i=0; i<nBars; i++) {
+            // create two triangles each time through
+            const offset = i*2;
+            indices.push(offset + 0, offset + 3, offset + 1);
+            indices.push(offset + 0, offset + 2, offset + 3);
+        }
+        */
+
+    // example 3: cylinder barrel
+    const nSlices = 50; // number of pie slices in the cylinder
+    for (let i=0; i<=nSlices; i++) {
+        // a goes 0..1 around the circle
+        const a = i / nSlices;
+        const angle = a * 2 * Math.PI;
+        const x = Math.cos(angle);
+        const z = Math.sin(-angle);
+
+        vertices.push(new gfx.Vector3(x, -1, z));
+        normals.push(new gfx.Vector3(x, 0, z));
+
+        vertices.push(new gfx.Vector3(x, 1, z));
+        normals.push(new gfx.Vector3(x, 0, z));
+    }
+
+    for (let i=0; i<nSlices; i++) {
+        // create two triangles each time through
+        const offset = i*2;
+        indices.push(offset + 0, offset + 3, offset + 1);
+        indices.push(offset + 0, offset + 2, offset + 3);
+    }
 
         mesh.setVertices(vertices);
+        mesh.setNormals(normals);
         mesh.setIndices(indices);
         return mesh;
     }
